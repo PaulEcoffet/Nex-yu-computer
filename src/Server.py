289f14c1faf2cfe6a32948010bac3f7199185173
Paul Ecoffet <ecoffet.paul@gmail.com>
2012-07-12 16:07:37 +0200

    Creation of the computer project
    Generated by eclipse

---
+++ src/Server.py
@@ -0,0 +1,8 @@
+class Server:
+    """Server which will receive the broadcast sent by
+    the application"""
+    def __init__(self):
+        """Init the server"""
+        self.test = "lol"
+        print("server init")
+
---
+++ src/main.py
@@ -0,0 +1,5 @@
+#!/bin/python
+
+from Server import Server
+
+server = Server()
