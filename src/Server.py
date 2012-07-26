bd76b267a2aa6b6d000e58a5038e994bf230dd23
Paul Ecoffet <ecoffet.paul@gmail.com>
2012-07-26 17:58:57 +0200

    Working on the server with Twisted, some experiments

--- src/Server.py
+++ src/Server.py
@@ -1,12 +1,13 @@
-import socket
+from twisted.internet import protocol
+from twisted.internet import reactor
+from twisted.protocols import basic
 
-class Server:
-    """Server which will receive the broadcast sent by
-    the application"""
-    
-    def __init__(self):
-        """Init the server"""
-        self.connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.connection.bind('', );
-        
+class FingerProtocol(basic.LineReceiver):
+    def lineReceived(self, line):
+        self.transport.write("No such user: {}\r\n".format(line))
+        self.transport.loseConnection()
+class FingerFactory(protocol.ServerFactory):
+    protocol = FingerProtocol
 
+reactor.listenTCP(1079, FingerFactory())
+reactor.run()
