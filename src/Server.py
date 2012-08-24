8f8ddd2a8e9e0929ced25dc40e27c5ae8f47fb94
Paul Ecoffet <ecoffet.paul@gmail.com>
2012-08-24 14:28:16 +0200

    Preparing to receive SMS to send

--- src/Server.py
+++ src/Server.py
@@ -3,7 +3,9 @@
 from twisted.internet import protocol
 from twisted.internet import reactor
 from twisted.protocols import basic
+from twisted.internet import stdio
 import json
+import string
 
 class NexYuServProtocol(basic.Int32StringReceiver):
     def stringReceived(self, line):
@@ -13,19 +15,30 @@ class NexYuServProtocol(basic.Int32StringReceiver):
         except:
             print("Wrong JSON")
         else:
-            if(data["type"] == "ping"):
-                send["type"] = "pong"
-            elif(data["type"] == "messages"):
+            if(data["type"] == "messages"):
                 messages = data["data"]
                 for message in messages:
                     print(message["sender"] + " sent " + message["body"])
                 send["type"] = "ok"
-
         self.sendString(json.dumps(send))
+    def sendSMS(self, number, body):
+        print("{} : {}".format(number, body))
+        
+    def connectionMade(self):
+        self.factory.io.server = self
+        print("Lol")
+
+
 
 class NexYuServFactory(protocol.ServerFactory):
     protocol = NexYuServProtocol
+    
+    def __init__(self, io):
+        self.io = io
 
+
+
+### FOR DEBUGGING PURPOSE
 class Echo(protocol.Protocol):
     def dataReceived(self, data):
         print(data)
@@ -34,7 +47,32 @@ class EchoFactory(protocol.Factory):
     def buildProtocol(self, addr):
         return Echo()
 
+class IO(basic.LineOnlyReceiver):
+    from os import linesep as delimiter #@UnusedImport
+    
+    def __init__(self):
+        self.server = None
+
+    def connectionMade(self):
+        self.transport.write('>>> ')
+
+    def lineReceived(self, line):
+        if self.server is not None:
+            words = string.split(line, " ")
+            number = words[0]
+            self.server.sendSMS(number, string.join(words[1:], " "))
+        else:
+            self.transport.write('Not connected yet.\n')
+        self.transport.write('>>> ')
+
+
+### END
+
+
 class Server:
     def __init__(self):
-        reactor.listenTCP(34340, NexYuServFactory()) #@UndefinedVariable
+        io = IO()
+        server = NexYuServFactory(io)
+        stdio.StandardIO(io)
+        reactor.listenTCP(34340, server) #@UndefinedVariable
         reactor.run() #@UndefinedVariable
