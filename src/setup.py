8fad2eba9c76ac8c125079cfd46c7ad04c4dcb1e
Paul Ecoffet <ecoffet.paul@gmail.com>
2012-10-20 16:41:20 +0200

    icon in taskbar is now displayed

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,12 @@
 import os
-
 if os.name == "nt":
     from cx_Freeze import setup, Executable
+    main = Executable("main.py", 
+					icon = "../res/icon.ico",
+					)
     setup(
         name = "Nex yu computer",
         version = "0.1",
         description = "Nex yu computer is the computer part of Nex yu",
-        executables = [Executable("main.py")],
+        executables = [main],
     )
